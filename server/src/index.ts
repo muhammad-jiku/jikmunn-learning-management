7d6a2c606e30257d132dcb6f1301b75586bda193
muhammad-jiku <muhammadjiku364@gmail.com>
2025-07-03 12:23:21 +0600

    server init and packages added

---
+++ server/src/index.ts
@@ -0,0 +1,42 @@
+import { clerkMiddleware, createClerkClient } from '@clerk/express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import * as dynamoose from 'dynamoose';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+/* CONFIGURATIONS */
+dotenv.config();
+const isProduction = process.env.NODE_ENV === 'production';
+if (!isProduction) {
+  dynamoose.aws.ddb.local();
+}
+
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(clerkMiddleware());
+
+/* ROUTES */
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+/* SERVER */
+const port = process.env.PORT || 3000;
+if (!isProduction) {
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}/`);
+  });
+}
