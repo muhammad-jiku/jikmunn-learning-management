66030432f7edad44003498c9724c3886bf65a0d2
muhammad-jiku <muhammadjiku364@gmail.com>
2025-07-06 20:15:27 +0600

    components added and util modified

--- client/src/components/shared/NonDashboardNavbar.tsx
+++ client/src/components/shared/NonDashboardNavbar.tsx
@@ -1,15 +1,20 @@
 'use client';
 
+import { SignedIn, SignedOut, UserButton, useUser } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 import { Bell, BookOpen } from 'lucide-react';
 import Link from 'next/link';
 
 const NonDashboardNavbar = () => {
+  const { user } = useUser();
+  const userRole = user?.publicMetadata?.userType as 'student' | 'teacher';
+
   return (
     <nav className='nondashboard-navbar'>
       <div className='nondashboard-navbar__container'>
         <div className='nondashboard-navbar__search'>
           <Link href='/' className='nondashboard-navbar__brand' scroll={false}>
-            EDROH
+            jikmunn
           </Link>
           <div className='flex items-center gap-4'>
             <div className='relative group'>
@@ -33,6 +38,39 @@ const NonDashboardNavbar = () => {
             <span className='nondashboard-navbar__notification-indicator'></span>
             <Bell className='nondashboard-navbar__notification-icon' />
           </button>
+
+          <SignedIn>
+            <UserButton
+              appearance={{
+                baseTheme: dark,
+                elements: {
+                  userButtonOuterIdentifier: 'text-customgreys-dirtyGrey',
+                  userButtonBox: 'scale-90 sm:scale-100',
+                },
+              }}
+              showName={true}
+              userProfileMode='navigation'
+              userProfileUrl={
+                userRole === 'teacher' ? '/teacher/profile' : '/user/profile'
+              }
+            />
+          </SignedIn>
+          <SignedOut>
+            <Link
+              href='/signin'
+              className='nondashboard-navbar__auth-button--login'
+              scroll={false}
+            >
+              Log in
+            </Link>
+            <Link
+              href='/signup'
+              className='nondashboard-navbar__auth-button--signup'
+              scroll={false}
+            >
+              Sign up
+            </Link>
+          </SignedOut>
         </div>
       </div>
     </nav>
